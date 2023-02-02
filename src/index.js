610e828d2cf61a561253bafc00721acc15ffada9
HellCAT0147 <letscheckmail23@gmail.com>
2023-02-02 21:06:58 +0500

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,17 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letters = [];
+    let translate = '';
+    for (let i = 0; i < expr.length; i += 10)
+        letters.push((+expr.slice(i, i+10)).toString());
+    letters = letters.map(str => {
+        return str.replace(/10/g, ".").replace(/11/g, "-").replace(/NaN/g, " ");
+    });
+    for (let letter of letters) {
+        translate += MORSE_TABLE[letter] || ' ';
+    }
+    return translate;
 }
 
 module.exports = {
